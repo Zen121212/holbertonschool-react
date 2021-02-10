8b158860777016534d910b8962d9c790d59f06e0
Zein Kassem <zein.s.kassem@gmail.com>
2021-02-10 23:56:15 +0200

    add

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -1,3 +1,6 @@
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
 const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
@@ -5,3 +8,6 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 const updatedRow: RowElement = { ...row, age: 23 };
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
