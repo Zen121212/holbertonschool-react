028967caafd432ee9943830e5a52afbeed864409
Zein Kassem <zein.s.kassem@gmail.com>
2021-02-18 23:41:30 +0200

    first commit

---
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from '../assets/holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          School dashboard
+        </h1>
+      </header>
+      <body className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+        <label htmlFor="email">
+          Enter your email:&nbsp;
+          <input type="email" id="email" name="email" />
+        </label>
+        &nbsp;
+        <label htmlFor="password">
+          Enter your password:&nbsp;
+          <input type="password" id="password" name="password" />
+        </label>
+
+        <button type="submit">OK</button>
+
+      </body>
+      <footer className="App-footer">
+        <p>
+          {`Copyright ${getFullYear()} - ${getFooterCopy(true)}`}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
