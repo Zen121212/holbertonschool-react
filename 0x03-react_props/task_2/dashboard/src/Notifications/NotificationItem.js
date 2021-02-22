cec25419dbd76ddaef1cc65f034782d69b182360
Zein Kassem <zein.s.kassem@gmail.com>
2021-02-22 20:00:02 +0200

    add

---
+++ 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const NotificationItem = ({type,html,value}) => {
+    if(html === undefined)
+    return <li data-notification-type={type}>{value}</li>;
+    else 
+    return (
+        <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
+    );
+}
+export default NotificationItem;
